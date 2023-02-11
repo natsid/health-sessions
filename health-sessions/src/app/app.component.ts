df3030f3c73b78b4969a585bea9e56e0a99c8b00
natsid <5732310+natsid@users.noreply.github.com>
2023-02-10 17:37:32 -0800

    create SessionService and have it fetch JSON data

--- health-sessions/src/app/app.component.ts
+++ health-sessions/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionsService } from './services/sessions.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'health-sessions';
+  
+  constructor(private sessionsService: SessionsService) { }
+
+  ngOnInit(): void {
+    this.sessionsService.getJson().subscribe({
+      next: (response) => console.log(response),
+      error: () => console.log('Request failed with error')
+    });
+  }
 }
