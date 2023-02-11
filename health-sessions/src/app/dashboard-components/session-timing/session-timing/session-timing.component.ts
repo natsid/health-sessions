8cd712d96e552641de628b0f7c5a1bfd27ed73d0
natsid <5732310+natsid@users.noreply.github.com>
2023-02-10 20:55:36 -0800

    SessionTimingComponent boilerplate

---
+++ health-sessions/src/app/dashboard-components/session-timing/session-timing/session-timing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'session-timing',
+  templateUrl: './session-timing.component.html',
+  styleUrls: ['./session-timing.component.less']
+})
+export class SessionTimingComponent {
+
+}
