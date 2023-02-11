0de62f74c728209d9f1997dec6210c5a4eb950ad
natsid <5732310+natsid@users.noreply.github.com>
2023-02-10 20:50:07 -0800

    DayOverviewComponent boilerplate

---
+++ health-sessions/src/app/dashboard-components/day-overview/day-overview/day-overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'day-overview',
+  templateUrl: './day-overview.component.html',
+  styleUrls: ['./day-overview.component.less']
+})
+export class DayOverviewComponent {
+
+}
