f35735ea792e7c1633a6034760cf99c4311d9d82
natsid <5732310+natsid@users.noreply.github.com>
2023-02-13 11:57:53 -0800

    reorganize Material modules

--- health-sessions/src/app/app.module.ts
+++ health-sessions/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DayOverviewComponent } from './dashboard-components/day-overview/day-overview/day-overview.component';
 import { SessionTimingComponent } from './dashboard-components/session-timing/session-timing/session-timing.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
